ddc24c679ad5fcdeaf856a3dfc398b1675953a5c
Ivano05 <ivan.augusto08@gmail.com>
2021-09-17 18:30:43 -0300

    creacion de objeto y clases

--- asados.py
+++ asados.py
@@ -1,13 +1,19 @@
-cantidad_de_personas = int(input("¿Entre cuantas personas se va a dividir?: "))
-amigos = []
-i = 0
-while i < cantidad_de_personas:
-   persona = input("Indique el nombre de un integrante: ")
-   monto = float(input("indique cunta plata puso: "))
-   integrante = {persona : monto}
-   amigos.append(integrante)
-   i = i+1
-#total = (sum(amigos.values()))
-print(total)
+class divicion:
+   def __init__(self):
+      self.amigos={}
+
+   def poniendo_plata(self):
+      cantidad_de_personas = int(input("¿Entre cuantas personas se va a dividir?: "))
+      i = 0
+      while i < cantidad_de_personas:
+         persona = input("Indique el nombre de un integrante: ")
+         monto = float(input("indique cunta plata puso: "))
+         integrante = {persona : monto}
+         self.amigos.append(integrante)
+         i = i+1
+
+
+
+
 
 
